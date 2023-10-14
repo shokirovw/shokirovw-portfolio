b32b4cfc0453cf1f82bb44649e387b38d7a62a85
shokirovw <shokirowv@gmail.com>
2023-10-14 22:25:22 +0500

    LCP image resised, title added, favicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,20 @@
+import { Metadata } from "next";
+
+// export const metadata: Metadata = {
+//     title: "Shokirov's Space"
+// }
+
 export default function RootestLayout ({ children }) {
     return (
-        <html>
-            <body>
-                {children}
-            </body>
+        <html lang="en">
+        <head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Shokirov's Space</title>
+        </head>
+        <body>
+            {children}
+        </body>
         </html>
     );
 }
